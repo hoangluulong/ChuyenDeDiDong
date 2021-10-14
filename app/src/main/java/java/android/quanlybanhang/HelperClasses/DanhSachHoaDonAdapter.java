ae7c91930985a6acd19e2bdf7a290433b5370b51
hoanghuulong <hoanghuulong.krn@gamil.com>
2021-10-14 19:36:38 +0700

    chức năng hiển thị danh sách

--- app/src/main/java/java/android/quanlybanhang/HelperClasses/DanhSachHoaDonAdapter.java
+++ app/src/main/java/java/android/quanlybanhang/HelperClasses/DanhSachHoaDonAdapter.java
@@ -1,7 +1,6 @@
 package java.android.quanlybanhang.HelperClasses;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.android.quanlybanhang.Model.PieTongQuan;
 import java.android.quanlybanhang.R;
-import java.util.List;
 
 public class DanhSachHoaDonAdapter extends RecyclerView.Adapter<DanhSachHoaDonAdapter.CustomChiSoSanPham> {
 
@@ -30,20 +27,53 @@ public class DanhSachHoaDonAdapter extends RecyclerView.Adapter<DanhSachHoaDonAd
 
     @Override
     public void onBindViewHolder(@NonNull CustomChiSoSanPham holder, int position) {
-
+        if (position == 0) {
+            holder.ngay.setText("13/10/2021");
+            holder.gio.setText("10:30 AM");
+            holder.gia.setText("240,000,00");
+            holder.ten.setText("Hoang Huu Long");
+        }else if (position == 1) {
+            holder.ngay.setText("13/10/2021");
+            holder.gio.setText("10:30 AM");
+            holder.gia.setText("300,000,00");
+            holder.ten.setText("Huu Long Hoang");
+        }else if (position == 2) {
+            holder.ngay.setText("12/10/2021");
+            holder.gio.setText("09:30 AM");
+            holder.gia.setText("100,000,00");
+            holder.ten.setText("Long Hoang Huu");
+        }else if (position == 3) {
+            holder.ngay.setText("20/10/2021");
+            holder.gio.setText("03:30 PM");
+            holder.gia.setText("100,000,00");
+            holder.ten.setText("Long Long Huu");
+        }else if (position == 4) {
+            holder.ngay.setText("30/09/2021");
+            holder.gio.setText("01:30 AM");
+            holder.gia.setText("90,000,00");
+            holder.ten.setText("Long Hoang Huu");
+        }
+        else if (position == 5) {
+            holder.ngay.setText("12/01/2021");
+            holder.gio.setText("09:30 AM");
+            holder.gia.setText("111,000,00");
+            holder.ten.setText("Long Hoang Huu");
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return 30;
     }
 
     public class CustomChiSoSanPham extends RecyclerView.ViewHolder {
-//        private TextView title, soLuong;
-
+        private TextView ngay, gio, gia, ten;
         public CustomChiSoSanPham(@NonNull View ItemView) {
             super(ItemView);
-
+            ngay = ItemView.findViewById(R.id.ngay);
+            gio = ItemView.findViewById(R.id.gio);
+            gia = ItemView.findViewById(R.id.gia);
+            ten = ItemView.findViewById(R.id.ten);
         }
     }
 }
