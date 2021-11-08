314ba615303905ea4976c06822c35bfbe2d92fbf
triettm40 <triettm.40@gmail.com>
2021-11-08 15:17:11 +0630

    Báo Cáo Kho

--- app/src/main/java/java/android/quanlybanhang/function/BaoCao/ChiTietNhapKhoActivity.java
+++ app/src/main/java/java/android/quanlybanhang/function/BaoCao/ChiTietNhapKhoActivity.java
@@ -1,27 +1,45 @@
 package java.android.quanlybanhang.function.BaoCao;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.android.quanlybanhang.HelperClasses.ChiTietNhapKhoAdapter;
+import java.android.quanlybanhang.HelperClasses.QuanLyKhoAdapter;
+import java.android.quanlybanhang.Model.BaoCaoKho;
+import java.android.quanlybanhang.Model.ChiTietNhapKho;
 import java.android.quanlybanhang.R;
+import java.util.List;
 
 public class ChiTietNhapKhoActivity extends AppCompatActivity {
-
+    public static final String KEY_BAOCAO = "BAOCAO";
     private ChiTietNhapKhoAdapter chiTietNhapKhoAdapter;
     private RecyclerView recyclerView;
     private Button back;
+    private TextView ngayGio,tenNhanVien;
+    private BaoCaoKho baoCaoKho;
+    private List<ChiTietNhapKho> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chi_tiet_nhap_kho);
         back = findViewById(R.id.back_btn);
+        ngayGio=findViewById(R.id.ngaygio);
+        tenNhanVien=findViewById(R.id.tennhanvien);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        baoCaoKho = (BaoCaoKho) bundle.getSerializable(QuanLyKhoAdapter.KEY_BAOCAO);
+        Log.d("triet", "onCreate: "+baoCaoKho.getNhanVien());
+        ngayGio.setText(baoCaoKho.getNgay()+" "+baoCaoKho.getGio());
+        tenNhanVien.setText(baoCaoKho.getNhanVien());
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +55,7 @@ public class ChiTietNhapKhoActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
 
-        chiTietNhapKhoAdapter = new ChiTietNhapKhoAdapter(this);
+        chiTietNhapKhoAdapter = new ChiTietNhapKhoAdapter(this,baoCaoKho.getChiTietNhapKho());
         recyclerView.setAdapter(chiTietNhapKhoAdapter);
 
         chiTietNhapKhoAdapter.notifyDataSetChanged();
